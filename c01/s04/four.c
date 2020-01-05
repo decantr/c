7560fb92dde4d5f60ad3ed1047189e1d9154cc30
decantr <theeasterlywood@gmail.com>
2020-01-05 15:47:20 +0000

    Add exercise 1.4

---
+++ c01/s04/four.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define LOWER	0
+#define UPPER	300
+#define INC		20
+
+/*
+	 print Fahrenheit-Celsius table
+*/
+void main()
+{
+	int far;
+
+	printf("Fahr Celsius\n");
+
+	for (far = LOWER; far <= UPPER; far = far + INC) {
+		printf("%3d %6.1f\n", far, (5.0/9.0) * (far - 32.0));
+	}
+}
