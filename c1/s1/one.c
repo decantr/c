82b51c70e91b5bc4bc257ccdac8dc8fd01095273
decantr <theeasterlywood@gmail.com>
2020-01-05 15:34:29 +0000

    Add exercise 1.1

---
+++ c1/s1/one.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	printf("Hello, world\n");
+
+	printf("Hello,");
+	printf("world");
+	printf("\n");
+
+	printf("Hello,\tworld\n");
+
+	printf("Hello,\mworld\n");
+}
