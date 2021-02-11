26cea50503f37946ca808d9817d83b8bd8e5d1c9
decantr <theeasterlywood@gmail.com>
2021-02-11 21:42:45 +0000

    one: add exersize seventeen

--- c01/s17/seventeen.c
+++ c01/s17/seventeen.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+#define MAXLINE 1000
+
+int getln(char line[], int maxline);
+
+/* print lines longer than 80 char */
+int main()
+{
+	int len;
+	char line[MAXLINE];
+
+	while ((len = getln(line, MAXLINE)) > 0)
+
+		if ( len > 80 )
+			printf("%s", line);
+}
+
+/* getline: read a line into s, return length */
+int getln(char s[], int lim)
+{
+	int c, i;
+
+	for ( i = 0;
+				i < lim - 1 && (c = getchar() ) != EOF && c != '\n';
+				++i )
+		s[i] = c;
+
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+
+	s[i] = '\0';
+	return i;
+}
